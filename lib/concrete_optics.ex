b88c7dffb6b8af84f27e99eda5ca2a1c26b39ec7
Sonat Suer <sonatsuer@gmail.com>
2025-10-05 15:53:13 +0200

    Elixir project template

---
+++ lib/concrete_optics.ex
@@ -0,0 +1,18 @@
+defmodule ConcreteOptics do
+  @moduledoc """
+  Documentation for `ConcreteOptics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConcreteOptics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/concrete_optics_test.exs
@@ -0,0 +1,8 @@
+defmodule ConcreteOpticsTest do
+  use ExUnit.Case
+  doctest ConcreteOptics
+
+  test "greets the world" do
+    assert ConcreteOptics.hello() == :world
+  end
+end
